eb91d35eb5cff1d03db511e964d83974228f8674
santiag0m <32421689+santiag0m@users.noreply.github.com>
2020-10-11 13:46:56 -0500

    React build

---
+++ build/precache-manifest.6e46b6ee97d7b701deb030ed4b38225c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a039cbe7c0f39ab476c07275e749ac77",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9481631e827e0ddd2639",
+    "url": "./static/css/main.1dd50786.chunk.css"
+  },
+  {
+    "revision": "cb6a085a823bd7e94204",
+    "url": "./static/js/2.de8613f6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.de8613f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9481631e827e0ddd2639",
+    "url": "./static/js/main.54c2b74d.chunk.js"
+  },
+  {
+    "revision": "a6cc593340d458bb32ea",
+    "url": "./static/js/runtime-main.a7c23ab5.js"
+  }
+]);

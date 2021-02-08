64c0eb5cf2327d8d4dcdf6c3fb3bab3309983be5
santiag0m <32421689+santiag0m@users.noreply.github.com>
2021-02-08 18:49:41 -0500

    'deploy'

---
+++ precache-manifest.1b975cc63a7a78cf6d3aca3cb7cb8f60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "809de062055b2a7c0fed90a38e0ca819",
+    "url": "/cardio-client/index.html"
+  },
+  {
+    "revision": "e6b18b6d628383ad3a01",
+    "url": "/cardio-client/static/css/main.e84966a6.chunk.css"
+  },
+  {
+    "revision": "2a827b1152bf59cd9b68",
+    "url": "/cardio-client/static/js/2.e681005e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/cardio-client/static/js/2.e681005e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6b18b6d628383ad3a01",
+    "url": "/cardio-client/static/js/main.7e2b0a85.chunk.js"
+  },
+  {
+    "revision": "e4cf72a28ca3341f38b2",
+    "url": "/cardio-client/static/js/runtime-main.95b7382a.js"
+  }
+]);

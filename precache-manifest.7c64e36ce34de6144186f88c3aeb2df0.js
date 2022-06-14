9cd13da11e8cce25024cb7c0cdaaa21a6c95e828
santiag0m <32421689+santiag0m@users.noreply.github.com>
2022-06-14 10:30:42 +0200

    Add build files

---
+++ precache-manifest.7c64e36ce34de6144186f88c3aeb2df0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04ad53ab793504d2713969776d84e9cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "695b83dc9bf263d38e18",
+    "url": "/static/css/main.e48cffbe.chunk.css"
+  },
+  {
+    "revision": "ed57add7bec0d39a067e",
+    "url": "/static/js/2.761ea93c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.761ea93c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "695b83dc9bf263d38e18",
+    "url": "/static/js/main.7f0f4cb2.chunk.js"
+  },
+  {
+    "revision": "9f1ffcfa80476881ab4f",
+    "url": "/static/js/runtime-main.b62c5f26.js"
+  }
+]);
